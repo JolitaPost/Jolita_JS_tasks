3b113afdaff877991865ebbd407709a5de56f319
Jolita Post <jolita.petkeviciute@gmail.com>
2022-09-08 21:37:56 +0300

    tenth task is completed

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,33 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const formElement = document.getElementsByName('form');
+const convertButton = document.getElementById('submit-btn');
+const inputElement = document.getElementById('search');
+const convertedResult = document.getElementById('output');
+/*
+formElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+})
+*/
+
+const lbAmount = 2.2046;
+const gAmount = 0.0010000;
+const ozAmount = 35.274;
+
+
+convertButton.addEventListener('click', function() {
+    const lbAmount = inputElement.value;
+    const kgAmount = convertToKg(lbAmount);
+    convertedResult.textContent = kgAmount;
+})
+
+
+function convertToKg(kgAmount) {
+    const result = kgAmount * lbAmount;
+    console.log(result);
+    return result;
+}
+
+const amountOfKg = convertToKg(5);
+console.log(amountOfKg);
